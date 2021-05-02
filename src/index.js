c54ff9fc9512a5958cdbc1ad62b60af2d1794862
RahulRaj <jokingboyrahul@gmail.com>
2021-05-02 12:29:23 +0530

    [Rahul] setting up with react babel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-const $ = require("jquery")
-$("#target").html("Hello World!!");
+import React from 'react'
+import {render} from 'react-dom'
+const Greeting =()=><h1>Hello From React</h1>
+
+render(
+    <Greeting/>,
+    document.getElementById('target')
+)
