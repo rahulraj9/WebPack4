bfb8544a6526b3ec444d5f45c54939b6dc9c8c75
RahulRaj <jokingboyrahul@gmail.com>
2021-05-02 13:17:10 +0530

    [Rahul] setting up of multipleentryPoint

---
+++ src/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const About = () => {
+    return(
+        <>
+        <h1>About React</h1>
+      
+    </>
+    )
+  
+}
+
+
+
+
+render(
+    <About />,
+    document.getElementById('target')
+)
---
+++ src/contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const Contact = () => {
+    return(
+        <>
+        <h1>contact React</h1>
+      
+    </>
+    )
+  
+}
+
+
+
+
+render(
+    <Contact />,
+    document.getElementById('target')
+)
